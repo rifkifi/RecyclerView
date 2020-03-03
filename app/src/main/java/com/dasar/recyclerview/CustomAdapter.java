921feafd98ab8ea037b89eac1f32a7d1df1b2895
Rifki Firdaus <rifkifi@gmail.com>
2020-03-04 05:57:33 +0700

    Initial commit

---
+++ app/src/main/java/com/dasar/recyclerview/CustomAdapter.java
@@ -0,0 +1,65 @@
+package com.dasar.recyclerview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+
+
+
+    ArrayList personNames;
+    Context context;
+
+    public CustomAdapter(Context context, ArrayList personNames) {
+        this.context = context;
+        this.personNames = personNames;
+    }
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // infalte the item Layout
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.rowlayout, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        MyViewHolder vh = new MyViewHolder(v); // pass the view to View Holder
+        return vh;
+    }
+
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        // set the data in items
+        holder.name.setText((String) personNames.get(position));
+        // implement setOnClickListener event on item view.
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // display a toast with person name on item click
+                Toast.makeText(context, (String) personNames.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    @Override
+    public int getItemCount() {
+        return personNames.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView name;// init the item view's
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            // get the reference of item view's
+            name = (TextView) itemView.findViewById(R.id.name);
+        }
+    }
+}
+
---
+++ app/src/main/java/com/dasar/recyclerview/MainActivity.java
@@ -0,0 +1,29 @@
+package com.dasar.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    // ArrayList for person names
+    ArrayList personNames = new ArrayList<>(Arrays.asList("Person 1", "Person 2", "Person 3", "Person 4", "Person 5", "Person 6", "Person 7"));
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // get the reference of RecyclerView
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        // set a LinearLayoutManager with default vertical orientation
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        //  call the constructor of CustomAdapter to send the reference and data to Adapter
+        CustomAdapter customAdapter = new CustomAdapter(MainActivity.this, personNames);
+        recyclerView.setAdapter(customAdapter); // set the Adapter to RecyclerView
+    }
+}
